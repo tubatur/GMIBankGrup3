438e936f76e1a5ff9baed26f781f4bf2761f3a53
Jasi <mzng3243@gmail.com>
2023-04-02 11:37:11 +0200

    ilk acilis

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"84f57abf-52c2-42d2-b0ab-a780afdce413","feature":"Login page should not be accessible with invalid credentials","scenario":"User try to open page with dont suitable information and page should offer some posibilties","start":1680391739878,"group":1,"content":"","tags":"@us005,","end":1680391740260,"className":"failed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
