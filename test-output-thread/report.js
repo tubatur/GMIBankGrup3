41ca27f6a08f810b6be5ce0c4ef2d27ec5fca7b1
Jasi <mzng3243@gmail.com>
2023-04-02 23:58:08 +0200

    Us005 tamamlandi

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"0e47605b-6ef5-442e-a9bf-b8949c396176","feature":"Login page should not be accessible with invalid credentials","scenario":"User try to open page with dont suitable information and page should offer some posibilties","start":1680430755962,"group":1,"content":"","tags":"@us005,","end":1680430843006,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
