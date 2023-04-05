86afdf098938818f9dc525dc5ede76964b2e5e12
Jasi <mzng3243@gmail.com>
2023-04-05 11:10:03 +0200

    Us012 tamamlandi

--- test-output-thread/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"5baa8637-9ee2-4c25-a21b-732dc916428e","feature":"An Employee can manage Customer","scenario":"By logging into the website as \"Employee\", viewing account information and checking the function of buttons such as edit,delete","start":1680683505542,"group":1,"content":"","tags":"@us012,","end":1680683559601,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
-});
