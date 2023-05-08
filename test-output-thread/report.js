424f36db1c4979f935ef2eac5fe2105e4b010bef
tubatur <ztubatur@gmail.com>
2023-05-08 16:59:36 +0200

    Delete unnecessary files

--- test-output-thread/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"da4438be-b105-4b37-950d-a40c7491aa95","feature":"Delete Staff from list","scenario":"Delete a former staff with the help of the RestAssured class","start":1681295079729,"group":1,"content":"","tags":"@us025,","end":1681295163981,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
-});
